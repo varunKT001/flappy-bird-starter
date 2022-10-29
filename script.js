b8fe6bf51fe7d3e27fa29ac6b06a849b190736ed
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-10-29 22:46:30 +0530

    :rocket: Game starter completed

---
+++ script.js
@@ -0,0 +1,20 @@
+// Game states
+let GRAVITY = 5;
+let BIRD_POS = 0;
+let JUMP = 50;
+let SCORE = 0;
+
+// Game elements
+let bird = document.querySelector('.bird');
+let pipes = [...document.querySelectorAll('.pipe')];
+let score = document.querySelector('.score span');
+
+function init() {}
+
+function play() {}
+
+function collided(source, target) {}
+
+function gameOver() {}
+
+window.onload = init;
